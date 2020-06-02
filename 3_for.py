6574c5a25648c78eea8e3d310a282a47f4d5c68e
Nadezhda Orlova <orlova113@rambler.ru>
2020-06-02 09:06:43 +0300

    Добавила класс без оценок

--- 3_for.py
+++ 3_for.py
@@ -1,7 +1,4 @@
-school_scores = [{'school_class': '4', 'scores': [3,4,4,5,2]}, {'school_class': '3', 'scores': [3,5,4,5,3]}, 
-{'school_class': '2', 'scores': [4,5,4,5,3]}, 
-{'school_class': '1', 'scores': [4,5,4,5,5]}
-]
+
 """
 Домашнее задание №1
 Цикл for: Оценки
@@ -11,23 +8,27 @@ school_scores = [{'school_class': '4', 'scores': [3,4,4,5,2]}, {'school_class': 
 * Посчитать и вывести средний балл по каждому классу.
 """
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
+    school_scores = [{'school_class': '4', 'scores': [3,4,4,5,2]}, {'school_class': '3', 'scores': [3,5,4,5,3]}, 
+    {'school_class': '2', 'scores': [4,5,4,5,3]}, 
+    {'school_class': '1', 'scores': [4,5,4,5,5]},
+    {'school_class': '5', 'scores': []}
+    ]
     sum_school = 0
     value_school = 0
     for i in range(len(school_scores)):
-        sum_class = 0
+        try:
+            sum_class = 0
         
-        for score in school_scores[i]['scores']:
-            sum_class += score
-        average_value = sum_class/len(school_scores[i]['scores'])
-        class_number = school_scores[i]['school_class']
-        print(f'Средний балл {class_number} класса: {average_value}')
+            for score in school_scores[i]['scores']:
+                sum_class += score
+            class_number = school_scores[i]['school_class']
+            average_value = sum_class/len(school_scores[i]['scores'])
+            print(f'Средний балл {class_number} класса: {average_value}')
             
-        sum_school += sum_class
-        value_school += len(school_scores[i]['scores'])
+            sum_school += sum_class
+            value_school += len(school_scores[i]['scores'])
+        except ZeroDivisionError:
+            print(f'Средний балл {class_number} класса: Начало четверти, еще нет оценок')
             
     average_value_school  =  sum_school/value_school
     print(f'Средняя оценка по школе: {average_value_school}')
